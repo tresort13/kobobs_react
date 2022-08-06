0ac831b6790b7ad36ae1aa05f49fb7824eb30b48
Mac <mac@MacBook-Pro-de-Mac.local>
2022-08-06 14:17:55 +0100

    commit9

--- src/pages/MenuEnvoi.js
+++ src/pages/MenuEnvoi.js
@@ -36,7 +36,7 @@ function MenuEnvoi(props)
         <Col xs = {"auto"}>
         <Link to="/form_envoie_client" style={{color:'white',textDecorationLine:'none'}}>
         <Button variant='secondary'  style={{width:400,height:80}} className='btn-lg rounded-pill zoom'>
-        <i className='couleur2'>Envoie Argent Client</i>
+        <i className='text-dark'>Envoie Argent Client</i>
         </Button>
         </Link>
         </Col>
@@ -47,7 +47,7 @@ function MenuEnvoi(props)
     <Row className='justify-content-center pb-3'>
         <Col xs = {"auto"}>
         <Link to="/form_envoie_client" style={{color:'white',textDecorationLine:'none'}}>
-        <Button variant='secondary'  style={{width:400,height:80}} className='btn-lg rounded-pill zoom'>
+        <Button variant='white'  style={{width:400,height:80}} className='btn-lg rounded-pill zoom'>
         <i className='couleur2'>Envoie Argent Abonné</i>
         </Button>
         </Link>
