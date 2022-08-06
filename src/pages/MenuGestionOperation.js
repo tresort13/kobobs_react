9361c8cee6c365fa38e2a1a6a20a9ab062f60fd9
Mac <mac@MacBook-Pro-de-Mac.local>
2022-08-06 04:33:59 +0100

    commit5

--- src/pages/MenuGestionOperation.js
+++ src/pages/MenuGestionOperation.js
@@ -4,7 +4,6 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Image from 'react-bootstrap/Image';
-import  './Login.css';
 import {Link} from  'react-router-dom';
 import Header from './Header';
 import Footer from './Footer';
