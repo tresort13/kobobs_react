6583fc391a5f007c7e8ccf5b8d26dc4f2f2de06e
Mac <mac@MacBook-Pro-de-Mac.local>
2022-08-22 19:42:33 -0700

    first commit11

--- src/pages/RetraitInfo.js
+++ src/pages/RetraitInfo.js
@@ -257,25 +257,23 @@ function MyVerticallyCenteredModal(props) {
   return (
     <Modal
       {...props}
-      size="lg"
+      size="md"
       aria-labelledby="contained-modal-title-vcenter"
       centered
     >
       <Modal.Header closeButton>
         <Modal.Title id="contained-modal-title-vcenter">
-          Modal heading
+          Echec de Validation
         </Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        <h4>Centered Modal</h4>
-        <p>
-          Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
-          dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
-          consectetur ac, vestibulum at eros.
+        <h4>Status Code Retrait : </h4>
+        <p className='text-danger'>
+         Ce code de retrait n'est pas encore validé !!!
         </p>
       </Modal.Body>
       <Modal.Footer>
-        <Button onClick={props.onHide}>Close</Button>
+        <Button onClick={props.onHide}>Fermer</Button>
       </Modal.Footer>
     </Modal>
   );
