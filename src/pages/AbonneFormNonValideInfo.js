7ac1a3a6281a7895214b07552c0aa5535730e664
Mac <mac@MacBook-Pro-de-Mac.local>
2022-08-23 10:48:50 -0700

    first commit11

--- src/pages/AbonneFormNonValideInfo.js
+++ src/pages/AbonneFormNonValideInfo.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{ useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Form from 'react-bootstrap/Form';
 import Button from "react-bootstrap/Button";
