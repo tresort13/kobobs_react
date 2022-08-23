7308e3d8d371ff432a2a9e16f7dd8be68a4e8541
Mac <mac@MacBook-Pro-de-Mac.local>
2022-08-23 10:41:31 -0700

    first commit11

--- src/pages/AbonneFormNonValideInfo.js
+++ src/pages/AbonneFormNonValideInfo.js
@@ -18,16 +18,8 @@ const useState = React.useState
 function AbonneFormNonValideInfo(props)
 {
 
-    const[barcode,setBarcode] = useState({infoBarcode :{
-        barcodeBagage:"",
-        operation:"",
-        position:"",
-        volInfo:""
-    }})
-
-    
-        const[position,setPosition] = useState('')
-
+    const [envoie4,setEnvoie4] = useState([])
+       
     const [message,setMessage] = useState("Liste des formulaires non validés")
     const [couleur,setCouleur] = useState("text-dark")
 
@@ -43,63 +35,39 @@ function AbonneFormNonValideInfo(props)
     
 
 
-    const submitBarcode = (e)=>
-    {
-        
-        e.preventDefault()
-        
-        fetch('https://kobobsapi.herokuapp.com/api/getRetraitNonValideInfo/code retrait en attente de validation/', {
-                method: 'PUT',
-                headers: {'Content-Type': 'application/json'},
-                body: JSON.stringify(barcode.infoBarcode)
-              })
-              .then( res => res.json())
-              .then(
-                res => {   
-                    if(res =="ok")
-                {
-                    
-                    setMessage("le barcode" +barcode.infoBarcode.barcodeBagage+" est scanné avec succès !!!")
-                    setCouleur("text-success")
-                } 
-                else{
-                    setMessage("echec operation")
-                    setCouleur("text-danger")
-                }
-       
-                }
-              )
-              .catch( (error) =>
-                {
-                    
-                    setMessage("echec operation")
-                    setCouleur("text-danger")
-                } )
-
-                console.log(barcode.infoBarcode.operation +" "+barcode.infoBarcode.position)
+      const submit = (e)=>
+      {
                 
+          fetch('https://kobobsapi.herokuapp.com/api/getRetraitNonValideInfo/code retrait en attente de validation/', {
+                  method:'GET',
+                  headers: {'Content-Type': 'application/json'},
+                 // body: JSON.stringify(codeRetrait.infoCodeRetrait)
+                })
+                .then( res => res.json())
+                .then(
+                  res => {   
+                    setEnvoie4(res)
+                     console.log(res)
+                  }
+                )
+                .catch( (error) =>
+                  {
+                      console.log(error)
+                  } )
+  
+      }
 
-                setBarcode({infoBarcode:{barcodeBagage:""}})
-    }
-
-    const inputChanged = (event)=>
-    {
-        const cred = barcode.infoBarcode;
-        cred[event.target.name] = event.target.value;
-        cred['operation'] = props.operationBG
-        cred['position'] = props.positionBG
-        cred['volInfo'] = props.volInfo
-        setBarcode({infoBarcode:cred})
-    }
-
-   
-
+      useEffect(()=>
+      {
+         const interval =  setInterval(()=>submit(),1000);
+          return () => clearInterval(interval)
+      },[envoie4])
    
     return (
         
         <>
         <Header username={props.username} />
-{isDesktop && props.envoie4.map((value)=>
+{isDesktop && envoie4.map((value)=>
     {
     return <Container className='bg-dark justify-content-center text-center bordure mb-5' style={{marginTop:50,width:1000}} >
 <Row className='justify-content-center mb-3 pt-3' >
@@ -155,149 +123,59 @@ function AbonneFormNonValideInfo(props)
 } 
 
 
-{isMobileOrTablet && <Container className='bg-dark my-auto mx-auto justify-content-center text-center bordure mb-5' style={{marginTop:50,backgroundColor:'white'}} >
+{isMobileOrTablet && props.envoie4.map((value)=>
+    {
+    return <Container className='bg-dark justify-content-center text-center bordure mb-5' style={{marginTop:50,width:1000}} >
 <Row className='justify-content-center mb-3 pt-3' >
-        <Col xs={"auto"}>
-        <p className={couleur}><i><b>{message}</b></i></p>
+        <Col xs={6}>
+        <p className='couleur2'><i><b>{message}</b></i></p>
         </Col>
     </Row>
 
-    <Row className='justify-content-center pb-2' >
-        <Col xs={"auto"}>
+    <Row className='justify-content-center pb-3' >
+        <Col xs={6}>
             <Link to="/tracer_baggages">
-            <Image src={require('./kobo_logo.JPG')}  className='rounded-pill ' style={{width:100}}></Image>
+            <Image src={require('./kobo_logo.JPG')}  className='rounded-pill ' style={{width:130}}></Image>
             </Link>
         
         </Col>
     </Row>
-    
-<Form>
-<Row className='justify-content-center'>
-        <Col xs = {4}>
-        <Form.Group className="mb-3" controlId="formBasicText" >
-        <Form.Control name="barcodeBagage" value={barcode.infoBarcode.barcodeBagage} onChange={e=>inputChanged(e)} type="text" required/>
-         </Form.Group>
-        </Col>
 
-        <Col xs = {4}>
-        <Form.Group className="mb-3" controlId="formBasicText" >
-        <Form.Control name="barcodeBagage" value={barcode.infoBarcode.barcodeBagage} onChange={e=>inputChanged(e)} type="text"  required/>
-         </Form.Group>
-        </Col>
-
-        <Col xs = {4}>
-        <Form.Group className="mb-3" controlId="formBasicText" >
-        <Form.Control name="barcodeBagage" value={barcode.infoBarcode.barcodeBagage} onChange={e=>inputChanged(e)} type="text"  required/>
-         </Form.Group>
-        </Col>
-    </Row>
-
-    <Row className='justify-content-center'>
-        <Col xs = {4}>
-        <Form.Group className="mb-3" controlId="formBasicText" >
-        <Form.Control name="barcodeBagage" value={barcode.infoBarcode.barcodeBagage} onChange={e=>inputChanged(e)} type="text"  required/>
-         </Form.Group>
-        </Col>
-
-        <Col xs = {4}>
-        <Form.Group className="mb-3" controlId="formBasicText" >
-        <Form.Control name="barcodeBagage" value={barcode.infoBarcode.barcodeBagage} onChange={e=>inputChanged(e)} type="text"  required/>
-         </Form.Group>
-        </Col>
-
-        <Col xs ={4}>
-        <Form.Group className="mb-3" >
-        <Form.Select name='position' value={position} aria-label="Default select example" onChange={e=>setPosition(e.target.value)} required>
-         <option>Selectionnez votre position</option>
-         <option value="ok_bagage_debarquement_arrivee">debarquement (arrivée)</option>
-         <option value="ok_bagage_en_tapis_livraison">tapis livraison</option>
-         <option value="ok_bagage_livrer">livrer bagage</option>
-         <option value="ok_bagage_stocke_arrivee">stocker bagage</option>
-         
-         </Form.Select>
-         </Form.Group>
-        </Col>
-    </Row>
-
-    <Row className='justify-content-center'>
-        <Col xs = {4}>
-        <Form.Group className="mb-3" controlId="formBasicText" >
-        <Form.Control name="barcodeBagage" value={barcode.infoBarcode.barcodeBagage} onChange={e=>inputChanged(e)} type="text"  required/>
-         </Form.Group>
-        </Col>
-
-        <Col xs = {4}>
-        <Form.Group className="mb-3" controlId="formBasicText" >
-        <Form.Control name="barcodeBagage" value={barcode.infoBarcode.barcodeBagage} onChange={e=>inputChanged(e)} type="text"  required/>
-         </Form.Group>
-        </Col>
-
-        <Col xs ={4}>
-        <Form.Group className="mb-3" >
-        <Form.Select name='position' value={position} aria-label="Default select example" onChange={e=>setPosition(e.target.value)} required>
-         <option>Selectionnez votre position</option>
-         <option value="ok_bagage_debarquement_arrivee">debarquement (arrivée)</option>
-         <option value="ok_bagage_en_tapis_livraison">tapis livraison</option>
-         <option value="ok_bagage_livrer">livrer bagage</option>
-         <option value="ok_bagage_stocke_arrivee">stocker bagage</option>
-         
-         </Form.Select>
-         </Form.Group>
-        </Col>
-    </Row>
-  
-    <Row className='justify-content-center'>
-        <Col xs = {4}>
-        <Form.Group className="mb-3" controlId="formBasicText" >
-        <Form.Control name="barcodeBagage" value={barcode.infoBarcode.barcodeBagage} onChange={e=>inputChanged(e)} type="text"  required/>
-         </Form.Group>
-        </Col>
-
-        <Col xs ={4}>
-        <Form.Group className="mb-3" >
-        <Form.Select name='position' value={position} aria-label="Default select example" onChange={e=>setPosition(e.target.value)} required>
-         <option>Selectionnez votre position</option>
-         <option value="ok_bagage_debarquement_arrivee">debarquement (arrivée)</option>
-         <option value="ok_bagage_en_tapis_livraison">tapis livraison</option>
-         <option value="ok_bagage_livrer">livrer bagage</option>
-         <option value="ok_bagage_stocke_arrivee">stocker bagage</option>
-         
-         </Form.Select>
-         </Form.Group>
-        </Col>
-
-        <Col xs ={4}>
-        <Form.Group className="mb-3" >
-        <Form.Select name='position' value={position} aria-label="Default select example" onChange={e=>setPosition(e.target.value)} required>
-         <option>Selectionnez votre position</option>
-         <option value="ok_bagage_debarquement_arrivee">debarquement (arrivée)</option>
-         <option value="ok_bagage_en_tapis_livraison">tapis livraison</option>
-         <option value="ok_bagage_livrer">livrer bagage</option>
-         <option value="ok_bagage_stocke_arrivee">stocker bagage</option>
-         
-         </Form.Select>
-         </Form.Group>
-        </Col>
-    </Row>
-  
-  
     <Row className='justify-content-center pb-3'>
-        <Col xs ={4}>
-        
-        <Link to="/home" style={{color:'white',textDecorationLine:'none'}}>
-        <Button variant="outline-warning" type="submit" onClick={e=>submitBarcode(e)}>
-        Valider Informations
+      <hr style={{color:"darkorange"}}></hr>
+      <p className='couleur2'><b><u>Informations générales </u></b> </p>
+    </Row>
+    <Row className='justify-content-center pb-3' >
+    <Col xs={12}>
+        <p className='text-light'>Kombo Mobimba ya Motindi (Noms complets Expediteur): <b className='couleur2'>{value.nom_expediteur} {value.postnom_expediteur} {value.prenom_expediteur}</b> </p>
+        <p className='text-light'>Kombo Mobimba ya Mozui (Noms complets Beneficiare): <b className='couleur2'>{value.nom_beneficiaire} {value.postnom_beneficiaire} {value.prenom_beneficiaire}</b> </p>
+        <p className='text-light'>Ekolo Motindi (Pays Expediteur) : <b className='couleur2'> {value.pays_expediteur}</b></p>
+        <p className='text-light'>Ekolo Mozui(Pays Beneficiare) : <b className='couleur2'> {value.pays_beneficiaire}</b></p>
+        <p className='text-light'>Mbongo Bazui (Montant Récuperé): <b className='couleur2'>{Number(value.montant_beneficiaire).toFixed(2)}</b> </p>
+        <p className='text-light'>Nzela yako zwa Mbongo (Type de retrait): <b className='couleur2'>{value.type_service}</b> </p>
+        <p className='text-light'>Status Retrait : <b className='couleur2'>{value.status_retrait}</b> </p>
+        <p className='text-light'>opération retrait fait par : <b className='couleur2'> {props.username}</b></p>
+        <p className='text-light'>date et heure : <b className='couleur2'> {value.date_heure_operation}</b></p> 
+    </Col>
+    <Col xs={6}>
+        <Link to="/envoi_info" style={{color:'white',textDecorationLine:'none'}}>
+        <Button variant="warning" type="submit">
+        Valider le code de retrait
         </Button>
         </Link>
-
         </Col>
-    </Row>
-  
+        <Col xs={6}>
+        <Link to="/envoi_info" style={{color:'white',textDecorationLine:'none'}}>
+        <Button variant="warning" type="submit">
+        Click pour voir plus des détails
+        </Button>
+        </Link>
+        </Col>
+    </Row>  
 
+</Container> })
+} 
 
-
-</Form>
-</Container>}
 <Row className="mt-5">
           <Col md={12}>
             <p></p>
