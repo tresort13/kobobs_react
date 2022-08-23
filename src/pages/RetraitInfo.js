0d7099d1e7a6494925f71b511dfcd9a6afa4e721
Mac <mac@MacBook-Pro-de-Mac.local>
2022-08-22 19:19:11 -0700

    first commit11

--- src/pages/RetraitInfo.js
+++ src/pages/RetraitInfo.js
@@ -247,44 +247,32 @@ const navigate = useNavigate()
             <p></p>
           </Col>
         </Row>
-<MydModalWithGrid show={modalShow} onHide={() => setModalShow(false)} />
+<MyVerticallyCenteredModal show={modalShow} onHide={() => setModalShow(false)} />
 <Footer />
-        </>
-       
+</>     
     )
 }
 
-function MydModalWithGrid(props) {
+function MyVerticallyCenteredModal(props) {
   return (
-    <Modal {...props} aria-labelledby="contained-modal-title-vcenter">
+    <Modal
+      {...props}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
       <Modal.Header closeButton>
         <Modal.Title id="contained-modal-title-vcenter">
-          Using Grid in Modal
+          Modal heading
         </Modal.Title>
       </Modal.Header>
-      <Modal.Body className="show-grid">
-        <Container>
-          <Row>
-            <Col xs={12} md={8}>
-              .col-xs-12 .col-md-8
-            </Col>
-            <Col xs={6} md={4}>
-              .col-xs-6 .col-md-4
-            </Col>
-          </Row>
-
-          <Row>
-            <Col xs={6} md={4}>
-              .col-xs-6 .col-md-4
-            </Col>
-            <Col xs={6} md={4}>
-              .col-xs-6 .col-md-4
-            </Col>
-            <Col xs={6} md={4}>
-              .col-xs-6 .col-md-4
-            </Col>
-          </Row>
-        </Container>
+      <Modal.Body>
+        <h4>Centered Modal</h4>
+        <p>
+          Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
+          dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
+          consectetur ac, vestibulum at eros.
+        </p>
       </Modal.Body>
       <Modal.Footer>
         <Button onClick={props.onHide}>Close</Button>
@@ -292,6 +280,8 @@ function MydModalWithGrid(props) {
     </Modal>
   );
 }
+
+
 
 
 export default RetraitInfo;
