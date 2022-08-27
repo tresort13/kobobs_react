204ca37354e0ff3a50685913ad14de5da7e05789
Mac <mac@MacBook-Pro-de-Mac.local>
2022-08-26 20:55:30 -0700

    first commit13

--- src/pages/AbonneFormNonValideInfo.js
+++ src/pages/AbonneFormNonValideInfo.js
@@ -124,13 +124,15 @@ function AbonneFormNonValideInfo(props)
     </Row>
     <Row className='justify-content-center pb-3' >
     <Col xs={12}>
-        <p className='text-light'>Kombo Mobimba ya Motindi (Noms complets Expediteur): <b className='couleur2'>{value.nom_expediteur} {value.postnom_expediteur} {value.prenom_expediteur}</b> </p>
+        <p className='text-light'>Kombo Mobimba ya Motindi (Noms complets Expediteur): <b className='text-dark bg-warning'>{value.nom_expediteur} {value.postnom_expediteur} {value.prenom_expediteur}</b> </p>
         <p className='text-light'>Kombo Mobimba ya Mozui (Noms complets Beneficiare): <b className='couleur2'>{value.nom_beneficiaire} {value.postnom_beneficiaire} {value.prenom_beneficiaire}</b> </p>
         <p className='text-light'>Ekolo Motindi (Pays Expediteur) : <b className='couleur2'> {value.pays_expediteur}</b></p>
+        <p className='text-light'>Numéro ya Motindi (Numéro Expediteur) : <b className='text-dark bg-warning'> {value.numero_expediteur}</b></p>
+        <p className='text-light'>Email ya Motindi (Numéro Expediteur) : <b className='couleur2'> {value.email_expediteur}</b></p>
         <p className='text-light'>Ekolo Mozui(Pays Beneficiare) : <b className='couleur2'> {value.pays_beneficiaire}</b></p>
         <p className='text-light'>Mbongo yako zwa (Montant à Recuperer): <b className='text-dark bg-warning'>{Number(value.montant_beneficiaire).toFixed(2)} $</b> </p>
         <p className='text-light'>Mbongo yako futa (Montant à payer): <b className='text-dark bg-warning'>{Number(value.montant_total).toFixed(2)} £</b> </p>
-        <p className='text-light'>Nzela yako zwa Mbongo (Type de retrait): <b className='couleur2'>{value.type_service}</b> </p>
+        <p className='text-light'>Nzela yako zwa Mbongo (Type de retrait): <b className='text-dark bg-warning'>{value.type_service}</b> </p>
         <p className='text-light'>Code Retrait : <b className='text-dark bg-warning'>{value.code_retrait}</b> </p>
         <p className='text-light'>Status Retrait : <b className='text-dark bg-warning'>{value.status_retrait}</b> </p>
         <p className='text-light'>date et heure : <b className='couleur2'> {value.date_heure_operation}</b></p> 
@@ -174,13 +176,15 @@ function AbonneFormNonValideInfo(props)
     </Row>
     <Row className='justify-content-center pb-3' >
     <Col xs={12}>
-    <p className='text-light'>Kombo Mobimba ya Motindi (Noms complets Expediteur): <b className='couleur2'>{value.nom_expediteur} {value.postnom_expediteur} {value.prenom_expediteur}</b> </p>
+    <p className='text-light'>Kombo Mobimba ya Motindi (Noms complets Expediteur): <b className='text-dark bg-warning'>{value.nom_expediteur} {value.postnom_expediteur} {value.prenom_expediteur}</b> </p>
         <p className='text-light'>Kombo Mobimba ya Mozui (Noms complets Beneficiare): <b className='couleur2'>{value.nom_beneficiaire} {value.postnom_beneficiaire} {value.prenom_beneficiaire}</b> </p>
         <p className='text-light'>Ekolo Motindi (Pays Expediteur) : <b className='couleur2'> {value.pays_expediteur}</b></p>
+        <p className='text-light'>Numéro ya Motindi (Numéro Expediteur) : <b className='text-dark bg-warning'> {value.numero_expediteur}</b></p>
+        <p className='text-light'>Email ya Motindi (Numéro Expediteur) : <b className='couleur2'> {value.email_expediteur}</b></p>
         <p className='text-light'>Ekolo Mozui(Pays Beneficiare) : <b className='couleur2'> {value.pays_beneficiaire}</b></p>
         <p className='text-light'>Mbongo yako zwa (Montant à Recuperer): <b className='text-dark bg-warning'>{Number(value.montant_beneficiaire).toFixed(2)} $</b> </p>
         <p className='text-light'>Mbongo yako futa (Montant à payer): <b className='text-dark bg-warning'>{Number(value.montant_total).toFixed(2)} £</b> </p>
-        <p className='text-light'>Nzela yako zwa Mbongo (Type de retrait): <b className='couleur2'>{value.type_service}</b> </p>
+        <p className='text-light'>Nzela yako zwa Mbongo (Type de retrait): <b className='text-dark bg-warning'>{value.type_service}</b> </p>
         <p className='text-light'>Code Retrait : <b className='text-dark bg-warning'>{value.code_retrait}</b> </p>
         <p className='text-light'>Status Retrait : <b className='text-dark bg-warning'>{value.status_retrait}</b> </p>
         <p className='text-light'>date et heure : <b className='couleur2'> {value.date_heure_operation}</b></p> 
