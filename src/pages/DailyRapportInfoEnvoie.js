821e301f81fab76315e498b52285b1fd7e0bc2cd
Mac <mac@MacBook-Pro-de-Mac.local>
2022-09-07 21:19:00 -0700

    first commit14

--- src/pages/DailyRapportInfoEnvoie.js
+++ src/pages/DailyRapportInfoEnvoie.js
@@ -39,7 +39,7 @@ function DailyRapportInfoEnvoie(props)
 
      const nombre_envoie_valide = props.dailyRapport.filter((value)=>
      {
-       return value.status_retrait == "Code Retrait Payé" || value.status_retrait == "Code Retrait Valide"
+       return value.status_retrait == "Code Retrait Payé"
      }).reduce((total,value)=>
      {
        total = total + 1
