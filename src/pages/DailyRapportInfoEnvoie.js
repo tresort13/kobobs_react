2ee91acb2766eb5720c564a4b229711a7a372048
Mac <mac@MacBook-Pro-de-Mac.local>
2022-09-07 21:14:33 -0700

    first commit14

--- src/pages/DailyRapportInfoEnvoie.js
+++ src/pages/DailyRapportInfoEnvoie.js
@@ -39,7 +39,7 @@ function DailyRapportInfoEnvoie(props)
 
      const nombre_envoie_valide = props.dailyRapport.filter((value)=>
      {
-       return value.status_retrait =! "code retrait en attente de validation"
+       return value.status_retrait == "Code Retrait Payé" || value.status_retrait == "Code Retrait Valide"
      }).reduce((total,value)=>
      {
        total = total + 1
